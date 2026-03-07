e1db6f30c8ead4925933167b6b3e23402b26e319
Sarthak Gour <sarthakgour9@gmail.com>
2026-03-07 15:10:28 +0530

    Improved UI and add content for Home and Product

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -2,13 +2,11 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideHttpClient(withFetch()),
-    provideRouter(routes), 
-    provideClientHydration(withEventReplay())
+    provideRouter(routes)
   ]
 };
