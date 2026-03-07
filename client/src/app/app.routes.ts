84401ae1f27a3d74bf610459f02cb3a4316a7eb0
Sarthak Gour <sarthakgour9@gmail.com>
2026-03-07 12:06:27 +0530

    created navbar and some route pages

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Product } from './product/product';
+import { AddProduct } from './pages/add-product/add-product';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },
+  {
+    path: 'products',
+    component: Product,
+  },
+  {
+    path: 'about',
+    component: About,
+  },
+  {
+    path: 'add-product',
+    component: AddProduct,
+  },
+];
--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Product } from './product/product';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet , CommonModule,  Product],
+  imports: [RouterOutlet , CommonModule,  Product, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
